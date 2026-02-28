0c924ba51e7c6112747979dedfa2d6163ce39412
RTL Warrior <166101761+shindevinayak192@users.noreply.github.com>
2026-02-28 20:00:43 +0530

    Update uart_fifo.v

--- uart_fifo.v
+++ uart_fifo.v
@@ -1,82 +1,72 @@
-module uart_fifo (clk_in, rstn, data_in, push, pop, data_out, fifo_empty, fifo_full, count);
-
-    parameter depth = 16,
-              width = 8;
-
-    // -------------------------------------------------
-    // Port Declarations
-    // -------------------------------------------------
-    input clk_in;
-    input rstn; //active low synchronous reset
-    input push; //similar to write_en
-    input pop;  //similar to read_en
-    input [(width-1):0] data_in;
-
-    output fifo_empty; //high when fifo is empty
-    output fifo_full;  //high when fifo is full
-    output reg [width-1:0] data_out;
-    output reg [$clog2(depth+1)-1:0] count; //$clog2 is same as log2[N] that means log of N to the base 2.
-
-    // -------------------------------------------------
-    // Internal Signals
-    // -------------------------------------------------
-    reg [$clog2(depth)-1:0] ip_count;
-    reg [$clog2(depth)-1:0] op_count;
-
-    reg [width-1:0] mem [0:depth-1];
-
-    wire push_valid;
-    wire pop_valid;
-
-    // -------------------------------------------------
-    // Valid Operation Logic : Provides boundary protection. Prevents overflow and underflow.
-    // -------------------------------------------------
-    assign push_valid = push && (!fifo_full || pop);
-    assign pop_valid  = pop  && (!fifo_empty || push);
-
-    // -------------------------------------------------
-    // Sequential Logic
-    // -------------------------------------------------
-    always @(posedge clk_in or negedge rstn) 
+module uart_fifo(clk_in, rstn, data_in, push, pop, data_out, fifo_empty, fifo_full, count);
+	
+	parameter 	depth = 16,
+				width = 8;
+				
+	input clk_in;
+	input rstn;
+	input push;		//write enable 
+	input pop;		//read enable
+	input [(width - 1):0]data_in;
+	output fifo_empty, fifo_full;
+	output reg [(width - 1):0]data_out;
+	output reg [4:0]count;
+	
+	//Internal signals and registers
+	reg [3:0]ip_count; 		//write pointer
+	reg [3:0]op_count;		//read pointer
+	reg [(width - 1):0] mem [0:(depth - 1)];
+	
+	//Write Logic
+	always@(posedge clk_in or negedge rstn)
 		begin
-			if (!rstn) 	//here we are not resetting the whole memory to '0' bcoz it is not needed. FIFO correctness does NOT depend on memory being 'zero'.
-						//We use fifo_empty == (count == 0). So if FIFO is empty we will never allow 'pop'. So we never read uninitialized memory.
+			if(!rstn)
 				begin
-					ip_count <= 0;
-					op_count <= 0;
-					count    <= 0;
-					data_out <= 0;
+					ip_count		<= 		4'd0;
 				end
-			else 
+			else if(push && !fifo_full)
 				begin
-
-					// Write Operation
-					if (push_valid) begin
-						mem[ip_count] <= data_in;
-						ip_count <= ip_count + 1'b1;
-					end
-
-					// Read Operation
-					if (pop_valid) begin
-						data_out <= mem[op_count];
-						op_count <= op_count + 1'b1;
-					end
-
-					// Count Update
-					if (push_valid && !pop_valid)
-						count <= count + 1'b1;
-					else if (!push_valid && pop_valid)
-						count <= count - 1'b1;
-					else
-						count <= count;
-
+					mem[ip_count]	<= 		data_in;
+					ip_count		<=		ip_count	+	1'b1;
 				end
 		end
 	
-	  //-------------------------------------------------
-    // Status Flags
-    // -------------------------------------------------
-    assign fifo_empty = (count == 0);
-    assign fifo_full  = (count == depth);
-
+	//Read Logic
+	always@(posedge clk_in or negedge rstn)
+		begin
+			if(!rstn)
+				begin
+					data_out	<=		0;
+					op_count	<=		0;
+				end
+			else if(pop && !fifo_empty)
+				begin
+					data_out	<=		mem[op_count];
+					op_count	<=		op_count	+	1'b1;
+				end
+			else
+				data_out		<=		8'bz;
+		end
+	
+	//count Logic
+	always@(posedge clk_in or negedge rstn)
+		begin
+			if(!rstn)
+				begin
+					count		<=		0;
+				end
+			else
+				begin
+					case({push, pop})
+						2'b01:	if(!fifo_empty)		count	<=	count	-	1'b1;		//pop only
+						2'b10:	if(!fifo_full)		count	<=	count	+	1'b1;		//push only
+						2'b11:						count	<=	count;					//both push and pop
+						default:					count	<=	count;
+					endcase
+				end
+		end
+	
+	//Status Flags			
+	assign fifo_empty = (count == 5'd0);
+	assign fifo_full = (count == 5'd16);
 endmodule
